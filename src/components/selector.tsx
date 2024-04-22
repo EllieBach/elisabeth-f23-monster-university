29902c66bf2984016a0aeac2a9ea7e7188927d14
EllieBach <elisabeth.bach95@hotmail.com>
2024-04-22 10:34:29 +0200

    folders and files

---
+++ src/components/selector.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const selector = () => {
+  return (
+   <>
+   <p>Filtrera studenter</p>
+   <select>
+    <option></option>
+    <option></option>
+    <option></option>
+   </select>
+
+   </>
+  )
+}
+
+export default selector
